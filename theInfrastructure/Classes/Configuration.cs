1dc342daee8bcb2586b8c50eaf20c8f34617ab86
JP <juergenpott@googlemail.com>
2026-08-15 19:38:39 +0200

    #

--- theInfrastructure/Classes/Configuration.cs
+++ theInfrastructure/Classes/Configuration.cs
@@ -12,13 +12,13 @@ namespace theInfrastructure
     public class SystemConfiguration
     {
         public string Title { get; set; } = string.Empty;
-        public string Sprache { get; set; } = string.Empty;
+        public string Sprache { get; set; } = string.Empty; // definiert die Sprache der UI 
+        public string Currency { get; set; } = string.Empty; // legt die genutzte Währung im System fest 
         public bool Akzente { get; set; } = false; // weiße Akzente anzeigen oder nicht 
         public bool Trash { get; set; } = false; // zeigt den Papierkorb an oder nicht 
         public bool Archive { get; set; } = false; // zeigt das Archiv an 
         public bool TaskBar { get; set; } = true; // zeigt die Taskbar unten rechts an
         public bool Clock { get; set; } = true; // zeigt die Uhr an 
-
         public void Save()
         {
             SystemConfiguration config = this as SystemConfiguration;
